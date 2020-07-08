56085dedec7c294464ba18f00fcec363f4e92d28
dantavs <dantavs@google.com>
2020-07-08 00:18:32 -0300

    issue Search Servive added

---
+++ src/services/Jira/issueSearchService.ts
@@ -0,0 +1,34 @@
+import { config } from 'https://deno.land/x/dotenv/mod.ts'
+import * as base64 from "https://denopkg.com/chiefbiiko/base64/mod.ts";
+
+const getIssuesListFromJira = async (id: string) => {
+    const JiraBaseUrl = config().JIRABASEURL
+    const issueUrl = `${JiraBaseUrl}search`
+
+    const jiraToken: string = base64.fromUint8Array(new TextEncoder().encode(config().JIRATOKEN));
+
+    const bodyData: string = `{
+        "jql": "key = RDI-1"
+        ,"fields": [
+            "key"
+            ,"summary"
+        ]
+    }`
+
+    console.log('queryData: ', bodyData)
+
+    const res = await fetch (issueUrl, {
+        method: 'POST',
+        headers: {
+        'Authorization': `Basic ${jiraToken}`,
+        'Accept': 'application/json',
+        'Content-Type':'application/json'
+        },
+        body: bodyData
+    })
+
+    return res
+
+}
+
+export { getIssuesListFromJira }
